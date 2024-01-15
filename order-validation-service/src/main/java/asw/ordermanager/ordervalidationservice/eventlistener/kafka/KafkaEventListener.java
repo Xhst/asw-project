3ffa2ce55e31f9e0517066e0f27f4b2eda3ad398
Carlo <car.proserpio@stud.uniroma3.it>
2024-01-15 11:50:48 +0100

    renamed class AND added product kafka listener on product topic

--- order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/eventlistener/kafka/KafkaEventListener.java
+++ order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/eventlistener/kafka/KafkaEventListener.java
@@ -1,7 +1,7 @@
 package asw.ordermanager.ordervalidationservice.eventlistener.kafka;
 
 import asw.ordermanager.common.api.event.DomainEvent;
-import asw.ordermanager.ordervalidationservice.domain.OrderEventsConsumer;
+import asw.ordermanager.ordervalidationservice.domain.OrderEventConsumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,16 +10,25 @@ import org.springframework.stereotype.Component;
 import java.util.logging.Logger;
 
 @Component
-public class OrderEventsKafkaListener {
+public class KafkaEventListener {
 
     private final Logger logger = Logger.getLogger(this.getClass().toString());
 
     @Autowired
-    private OrderEventsConsumer orderEventConsumer;
+    private OrderEventConsumer orderEventConsumer;
+
+
 
     @KafkaListener(topics = "${asw.kafka.channel.order}", groupId = "${asw.kafka.order.group-id}")
-    public void listen(ConsumerRecord<String, DomainEvent> record) throws Exception {
+    public void listenOrderTopic(ConsumerRecord<String, DomainEvent> record) throws Exception {
         logger.info("ORDER EVENT LISTENER: " + record.toString());
+        DomainEvent event = record.value();
+        orderEventConsumer.onEvent(event);
+    }
+
+    @KafkaListener(topics = "${asw.kafka.channel.product}", groupId = "${asw.kafka.product.group-id}")
+    public void listenProductTopic(ConsumerRecord<String, DomainEvent> record) throws Exception {
+        logger.info("PRODUCT EVENT LISTENER: " + record.toString());
         DomainEvent event = record.value();
         orderEventConsumer.onEvent(event);
     }
