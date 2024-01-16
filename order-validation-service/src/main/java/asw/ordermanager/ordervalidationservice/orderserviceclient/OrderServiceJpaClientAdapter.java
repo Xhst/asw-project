f66bf051d01fc4ebdf96a94b64a79a30a0b9a0e8
Carlo <car.proserpio@stud.uniroma3.it>
2024-01-16 13:51:44 +0100

    created OrderServiceJpaClientAdapter adapter

---
+++ order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/orderserviceclient/OrderServiceJpaClientAdapter.java
@@ -0,0 +1,20 @@
+package asw.ordermanager.ordervalidationservice.orderserviceclient;
+
+import asw.ordermanager.ordervalidationservice.domain.Order;
+import asw.ordermanager.ordervalidationservice.domain.OrderRepository;
+import asw.ordermanager.ordervalidationservice.domain.OrderServiceClientPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service("order-service-client-jpa")
+public class OrderServiceJpaClientAdapter implements OrderServiceClientPort {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Override
+    public Order getOrder(Long id) {
+        return orderRepository.findById(id).orElse(null);
+    }
+}
