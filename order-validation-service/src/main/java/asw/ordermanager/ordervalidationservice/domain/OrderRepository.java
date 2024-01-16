82b118a67c7fd0696df461059e3a05809b753b43
Carlo <car.proserpio@stud.uniroma3.it>
2024-01-16 13:51:18 +0100

    created OrderRepository

---
+++ order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/domain/OrderRepository.java
@@ -0,0 +1,7 @@
+package asw.ordermanager.ordervalidationservice.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+}
