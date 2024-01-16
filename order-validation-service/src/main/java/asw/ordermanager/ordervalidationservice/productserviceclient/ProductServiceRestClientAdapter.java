f79f635013e1a9a1ab5e0aa1be7430aa5ebede71
Carlo <car.proserpio@stud.uniroma3.it>
2024-01-16 14:35:44 +0100

    typo on service name

--- order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/productserviceclient/ProductServiceRestClientAdapter.java
+++ order-validation-service/src/main/java/asw/ordermanager/ordervalidationservice/productserviceclient/ProductServiceRestClientAdapter.java
@@ -18,7 +18,7 @@ import java.util.stream.*;
 
 import java.util.logging.Logger; 
 
-@Service("order-service-client-rest")
+@Service("product-service-client-rest")
 public class ProductServiceRestClientAdapter implements ProductServiceClientPort {
 
 	@Autowired 
